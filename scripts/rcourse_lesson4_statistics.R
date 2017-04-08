fcbcfe23b7c7146975e5193b19d102e542aa780c
Alan Zelicoff <zalan8587@q.com>
2017-04-08 17:16:01 -0600

    Finished statistics scripts and put environment into write-up folder

---
+++ scripts/rcourse_lesson4_statistics.R
@@ -0,0 +1,34 @@
+## READ IN DATA ####
+source("scripts/rcourse_lesson4_cleaning.R")
+
+
+## LOAD PACKAGES ####
+
+
+## ORGANIZE THE DATA for modelling purposes ####
+
+data_stats <- data_clean %>%
+    mutate(series = factor(series, levels = c("tos", "tng")))
+
+## CREATE MODELS ####
+
+# Does the series predict extinction?
+extinct_series.glm <- glm(extinct ~ series, family = "binomial", data= data_stats)
+extinct_series.glm_sum <- summary(extinct_series.glm)
+
+# Does alignment with the Federation predit extinction?
+extinct_alignment.glm <-glm(data = data_stats, extinct ~ alignment, family = "binomial")
+extinct_alignment.glm_sum <-summary(extinct_alignment.glm)
+
+# Two variable additive
+extinct_seriesalignment.glm <- glm(extinct ~ series + alignment, family = "binomial", data = data_stats)
+extinct_seriesalignment.glm_sum <- summary(extinct_seriesalignment.glm)
+
+# Two variable interaction model
+extinct_seriesXalignment.glm <- glm(extinct ~ series * alignment, family = "binomial", data = data_stats)
+extinct_seriesXalignment.glm_sum <-summary(extinct_seriesXalignment.glm)
+
+# Two variables interaction (change baseline for series)
+extinct_seriesxalignment_tng.glm = glm(extinct ~ relevel(series, "tng") * alignment, family = "binomial", data = data_stats)
+
+extinct_seriesxalignment_tng.glm_sum = summary(extinct_seriesxalignment_tng.glm)
